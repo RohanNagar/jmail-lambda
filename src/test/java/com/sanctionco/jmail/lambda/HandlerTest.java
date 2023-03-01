2a05829649fc466a556f3b3aba95a6917a670a6a
Rohan Nagar <rohan.nagar@utexas.edu>
2023-02-28 16:49:24 -0800

    Add unit tests

---
+++ src/test/java/com/sanctionco/jmail/lambda/HandlerTest.java
@@ -0,0 +1,47 @@
+package com.sanctionco.jmail.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class HandlerTest {
+
+  @Test
+  void testValidEmailAddress() {
+    APIGatewayProxyRequestEvent event = new APIGatewayProxyRequestEvent();
+    event.setQueryStringParameters(Collections.singletonMap("address", "test@test.com"));
+
+    Context context = mock(Context.class);
+    when(context.getLogger()).thenReturn(mock(LambdaLogger.class));
+
+    Handler handler = new Handler();
+
+    APIGatewayProxyResponseEvent response = handler.handleRequest(event, context);
+
+    assertEquals(200, response.getStatusCode());
+  }
+
+  @Test
+  void testInvalidEmailAddress() {
+    APIGatewayProxyRequestEvent event = new APIGatewayProxyRequestEvent();
+    event.setQueryStringParameters(Collections.singletonMap("address", "test@-test.com"));
+
+    Context context = mock(Context.class);
+    when(context.getLogger()).thenReturn(mock(LambdaLogger.class));
+
+    Handler handler = new Handler();
+
+    APIGatewayProxyResponseEvent response = handler.handleRequest(event, context);
+
+    assertEquals(400, response.getStatusCode());
+  }
+}
